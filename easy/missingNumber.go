24d7795762ddd0a8eb0f348e61c809ef3af00462
AlexBeskrovniy <aleksandr.beskrovniy.90@gmail.com>
2024-08-14 08:00:15 +0500

    missing number more clear solution

--- easy/missingNumber.go
+++ easy/missingNumber.go
@@ -1,17 +1,34 @@
 package easy
 
+// func MissingNumber(nums []int) int {
+// 	m := make(map[int]bool)
+
+// 	for i := 0; i <= len(nums)-1; i++ {
+// 		m[nums[i]] = true
+// 	}
+
+// 	for k := 0; k <= len(nums)+1; k++ {
+// 		if !m[k] {
+// 			return k
+// 		}
+// 	}
+
+// 	return -1
+// }
+
+//More optimized way
+
 func MissingNumber(nums []int) int {
-	m := make(map[int]bool)
+	nums_sum := 0
+	expected_sum := 0
 
-	for i := 0; i <= len(nums)-1; i++ {
-		m[nums[i]] = true
-	}
-
-	for k := 0; k <= len(nums)+1; k++ {
-		if !m[k] {
-			return k
+	for i := 0; i <= len(nums); i++ {
+		if i < len(nums) {
+			nums_sum += nums[i]
 		}
+
+		expected_sum += i
 	}
 
-	return -1
+	return expected_sum - nums_sum
 }
